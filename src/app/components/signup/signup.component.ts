7fe8dfd43d3543e7bac44cdb568c1abff4c09eef
abhinay-developer <Sarala@736>
2022-03-24 09:43:09 +0530

    formarray

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+signupForm:FormGroup;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.onInitCreateSignupForm();
+  }
+
+  onInitCreateSignupForm(){
+    this.signupForm=this.fb.group({
+      'username':['',[Validators.required]],
+      'email':['',[Validators.required]],
+      'password':['',[Validators.required]],
+      'mobileNumber':['',[Validators.required]],
+       'address':this.fb.array([])
+    })
+  }
+
+  createAddress():FormGroup{
+    return this.fb.group({
+      'city':['',[Validators.required]],
+      'country':['',[Validators.required]],
+      'pincode':['',[Validators.required]],
+      'streetName':['',[Validators.required]]
+    })
+  }
+  address(): FormArray {
+    return this.signupForm.get('address') as FormArray;
+  }
+  addAddress(event:any){
+     const selectedEvent=event.target.checked;
+      if(selectedEvent){
+           this.address().push(this.createAddress());
+      }
+      else{
+        const index = this.address().controls.findIndex(x => x.value === event.target.value);
+         this.address().removeAt(index);
+      }
   }
 
 }
+
+   
